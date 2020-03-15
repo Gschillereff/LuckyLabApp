e9b523505e1082936daa90502a990dc44ca294a7
aegger <aegger@ebay.com>
2020-03-15 14:20:33 -0700

    converted main activity and count in activity to fragments

--- app/src/main/java/com/neongarage/Activities/CountInActivity.java
+++ app/src/main/java/com/neongarage/Activities/CountInActivity.java
@@ -1,145 +1,29 @@
 package com.neongarage.Activities;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
+import com.neongarage.Fragments.CountInFragment;
 import com.neongarage.R;
 
-import com.google.android.material.textfield.TextInputEditText;
-
 public class CountInActivity extends BaseActivity {
-
-    enum CurrencyValue {
-
-        NICKEL(.05), DIME(.1), QUARTER(.25), DOLLAR(1), FIVE(5), TEN(10), TWENTY(20);
-
-        CurrencyValue(double value) {
-            this.value = value;
-        }
-
-        double value;
-        int count;
-
-        public double getValue() {
-            return value;
-        }
-
-        public void setCount(int count) {
-            this.count = count;
-        }
-
-        public double getTotal() {
-            return count * getValue();
-        }
-    }
-
-    Button submitButton;
-    TextInputEditText nickelsEdit;
-    TextInputEditText dimesEdit;
-    TextInputEditText quartersEdit;
-    TextInputEditText onesEdit;
-    TextInputEditText fivesEdit;
-    TextInputEditText tensEdit;
-    TextInputEditText twentiesEdit;
-    TextView resultText;
-    TextView dollarTotal;
-    TextView coinTotal;
-    TextView grandTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.count_in_activity);
+        setContentView(R.layout.activity_host);
 
-        submitButton = findViewById(R.id.submit_button);
-    }
+        if (savedInstanceState == null) {
+            CountInFragment fragment = new CountInFragment();
+            // Intent extras and fragment Args are both of type android.os.Bundle.
+            fragment.setArguments(getIntent().getExtras());
 
-    /**
-     * This handles the submit button click
-     */
-    public void handleSubmitClick(View v) {
-
-        //Get the nickels
-        nickelsEdit = findViewById(R.id.nickles_edit);
-        CurrencyValue nickels = CurrencyValue.NICKEL;
-        nickels.setCount(parseInt(nickelsEdit, 0));
-
-        //Get the dimes
-        dimesEdit = findViewById(R.id.dimes_edit);
-        CurrencyValue dimes = CurrencyValue.DIME;
-        dimes.setCount(parseInt(dimesEdit, 0));
-
-        //Get the quarters
-        quartersEdit = findViewById(R.id.quarters_edit);
-        CurrencyValue quarters = CurrencyValue.QUARTER;
-        quarters.setCount(parseInt(quartersEdit, 0));
-
-        //Get the ones
-        onesEdit = findViewById(R.id.ones_edit);
-        CurrencyValue ones = CurrencyValue.DOLLAR;
-        ones.setCount(parseInt(onesEdit, 0));
-
-        //Get fives
-        fivesEdit = findViewById(R.id.fives_edit);
-        CurrencyValue fives = CurrencyValue.FIVE;
-        fives.setCount(parseInt(fivesEdit, 0));
-
-        //Get tens
-        tensEdit = findViewById(R.id.tens_edit);
-        CurrencyValue tens = CurrencyValue.TEN;
-        tens.setCount(parseInt(tensEdit, 0));
-
-        //Get twenties
-        twentiesEdit = findViewById(R.id.twenties_edit);
-        CurrencyValue twenties = CurrencyValue.TWENTY;
-        twenties.setCount(parseInt(twentiesEdit, 0));
-
-        double nickleTotal = nickels.getTotal();
-        double dimeTotal = dimes.getTotal();
-        double quarterTotal = quarters.getTotal();
-        double coinValue = nickleTotal + quarterTotal + dimeTotal;
-        coinTotal = findViewById((R.id.coin_view));
-        coinTotal.setText("Coin Total: " + coinValue);
-
-        //Count value of dollars
-        double onesTotal = ones.getTotal();
-        double fiveTotal = fives.getTotal();
-        double tenTotal = tens.getTotal();
-        double twentyTotal = twenties.getTotal();
-        double dollarTot = onesTotal + fiveTotal + tenTotal + twentyTotal;
-        dollarTotal = findViewById(R.id.dollar_view);
-        dollarTotal.setText("Dollar Total: " + dollarTot);
-
-        //Grand total
-        double grandTot = dollarTot + coinValue;
-        grandTotal = findViewById(R.id.total_view);
-        grandTotal.setText("Grand Total: " + grandTot);
-
-        //Difference
-        resultText = findViewById(R.id.result_view);
-        if (grandTot > 150.45) {
-            double difference = grandTot - 150.45;
-            resultText.setText("You are over\nDrop: " + Math.ceil(difference));
-        } else if (grandTot < 149.45) {
-            double difference = 149.95 - grandTot;
-            resultText.setText("You are under. \nAsk Gatekeeper for " + Math.ceil(difference));
-        } else
-            resultText.setText("You are right on the Money. Good Job");
-    }
-
-
-    public static int parseInt(TextInputEditText editText, int defaultValue) {
-        int intReturn;
-        String value = editText.getText().toString();
-        try {
-            intReturn = Integer.parseInt(value);
-        } catch (NumberFormatException ex) {
-            intReturn = defaultValue;
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.main_content, fragment)
+                    .commit();
         }
-        return intReturn;
     }
 }
+
 
 
--- app/src/main/java/com/neongarage/Activities/MainActivity.java
+++ app/src/main/java/com/neongarage/Activities/MainActivity.java
@@ -1,10 +1,8 @@
 package com.neongarage.Activities;
 
 import android.os.Bundle;
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
 
+import com.neongarage.Fragments.MainFragment;
 import com.neongarage.R;
 
 public class MainActivity extends BaseActivity {
@@ -13,32 +11,17 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_host);
 
-        Button countInButton = findViewById(R.id.count_in_button);
-        Button tipOutButton = findViewById(R.id.tip_out_button);
+        if (savedInstanceState == null) {
+            MainFragment fragment = new MainFragment();
+            // Intent extras and fragment Args are both of type android.os.Bundle.
+            fragment.setArguments(getIntent().getExtras());
 
-        countInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToSecondActivity();
-            }
-        });
-        tipOutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v1) {
-                goToThirdActivity();
-            }
-        });
-    }
-
-    private void goToSecondActivity() {
-        Intent intent = new Intent(this, CountInActivity.class);
-        startActivity(intent);
-    }
-
-    private void goToThirdActivity() {
-        Intent intent1 = new Intent(this, TipCountInActivity.class);
-        startActivity(intent1);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.main_content, fragment)
+                    .commit();
+        }
     }
 }
