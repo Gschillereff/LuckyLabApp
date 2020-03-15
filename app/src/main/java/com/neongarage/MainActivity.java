d2215b318cc542c6b78f3c4b4f14be62a3f6489c
aegger <aegger@ebay.com>
2020-03-14 17:20:48 -0700

    more clean up

--- app/src/main/java/com/neongarage/MainActivity.java
+++ app/src/main/java/com/neongarage/MainActivity.java
@@ -1,14 +1,12 @@
 package com.neongarage;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
-import com.neongarage.schil.helloworld.R;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,26 +16,27 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.count_in_button);
         Button button1 = findViewById(R.id.tip_out_button);
 
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 goToSecondActivity();
             }
         });
 
-        button1.setOnClickListener(new View.OnClickListener(){
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v1){
+            public void onClick(View v1) {
                 goToThirdActivity();
             }
         });
     }
-    private void goToSecondActivity(){
+
+    private void goToSecondActivity() {
         Intent intent = new Intent(this, CountInActivity.class);
         startActivity(intent);
     }
 
-    private void goToThirdActivity(){
+    private void goToThirdActivity() {
         Intent intent1 = new Intent(this, TipCountInActivity.class);
         startActivity(intent1);
     }
